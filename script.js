bfdd887f14fc29249f62da4f4c5aa75bfb32d45a
heroinme.heroinme <heroinme1@yandex.ru>
2017-07-19 21:23:38 +0400

    update

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
 	var bl = $('.cd').length;
 	while (bl < 256) {
 		$('div.content').append('<div class="cd"></div>');
-		console.log(bl);
 		bl = $('.cd').length;
 	}
+	$('.cd').mouseenter(function(){
+		$(this).css({"background-color":"yellow"})
+		$('.cd').mouseleave(function(){
+			$(this).css({"background-color":"black"})	
+		});
+	});
 });
+
+
+
